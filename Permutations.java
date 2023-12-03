865ef4459e78ff2d18ca3a61c3288521f3b7e55c
KING-258 <amulyaparashar258@gmail.com>
2023-12-03 18:22:28 +0530

    King

--- Permutations.java
+++ Permutations.java
@@ -1,8 +1,6 @@
 import java.util.*;
-
 class Permutations {
     static int m = 0;
-
     public static void Permutation(String str, String s1, int s) {
         if (str.length() == 0) {
             m++;
@@ -15,7 +13,6 @@ class Permutations {
             Permutation(newstr, s1 + first, s + 1);
         }
     }
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Input");
